40105b5c45028ca18a9e3ed36cb0b4789101c80f
mchavesferreira <63993080+mchavesferreira@users.noreply.github.com>
2025-02-28 11:19:33 -0300

    Create ex2_modificadores.c

---
+++ linguagemc/ex2_modificadores.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int valor;
+	float imposto=2.5;
+	valor = 100;
+	imposto =  imposto * valor;
+
+    printf("Hello world!\n");
+    printf("\timposto = %5.7f\n", imposto);
+    return 0;
+}
