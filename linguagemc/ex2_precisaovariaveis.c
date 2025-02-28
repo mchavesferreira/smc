0f7237fdd2dd81496491412f41ff8cc4c82c1f0c
mchavesferreira <63993080+mchavesferreira@users.noreply.github.com>
2025-02-28 11:28:38 -0300

    Update ex2_precisaovariaveis.c

--- linguagemc/ex2_precisaovariaveis.c
+++ linguagemc/ex2_precisaovariaveis.c
@@ -1,13 +1,15 @@
+
 #include <stdio.h>
 
 void main (void) {
-	char var8;
+	char var8=132;
 	int var16;
 	long int var32;
 	float pont16;
 	double pont32;
 	var8 = var8 + var16;     // 1
-    printf("\t variavel var8= %3.7f\n", var8);
+    printf("\t variavel var8= %d\n", var8);
+     // printf("\t variavel var8= %3.7f\n", var8);
 	var8 = var8 + var8;      // 2
 	var16 = var8 * var8;     // 3
 	var32 = var32 / var16;   // 4
@@ -16,5 +18,5 @@ void main (void) {
 	pont16 = pont32 * var32; // 7
 	pont16 = 40 / 80;        // 8
 
-  printf("\t variavel var8= %3.7f\n", var8);
+
 }
