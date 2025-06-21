bb0f703873f4e23a38907e8bae044a453820115a
mchavesferreira <63993080+mchavesferreira@users.noreply.github.com>
2025-06-21 16:50:33 -0300

    Update ESP32pedido.ino

--- exemplotrabalho2/ESP32pedido.ino
+++ exemplotrabalho2/ESP32pedido.ino
@@ -1,29 +1,21 @@
 /// versao 11/06/2025
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "index.h"  //Web page header file
+#include "index.h"  // pagina web
+#include "funcoes.h"  //variaveis e funcoes wifi
 
 WebServer server(80);
+// Variaveis
 int i = 0;
-
-// LED na porta 27
 const int ledPin = 19;
 const int ledPin2 = 4;
-
+unsigned long tempoAtual;
 // Comunicação Serial
 const int serialBaudRate = 115200;
 const int serialDataTimeout = 1000;  // Tempo de espera para receber dados via serial (1 segundo)
-
-// Enter your SSID and PASSWORD
-const char* ssid = "ifsp-ibge-1";
-const char* password = "ifspcatanduva";
-
-
-
 
 // OLED
 #define SCREEN_WIDTH 128
@@ -40,9 +32,12 @@ bool etapaFimprocesso = false;
 bool pedidoCancelado = false;
 
 String mensagemPedido = ""; // Variável para armazenar a mensagem do pedido finalizado
+String sabor = "";
+String tamanho = "";
+String calda = "";
 
 //===============================================================
-// This routine is executed when you open its IP in a browser
+// funcoes respondem a pagina web
 //===============================================================
 void handleRoot() {
   String s = MAIN_page; // Read HTML contents
@@ -50,63 +45,52 @@ void handleRoot() {
   server.send(200, "text/html", s); // Send web page
 }
 
-String sabor = "";
-String tamanho = "";
-String calda = "";
-
-
 void handleFazerPedido() {
   sabor = server.arg("sabor");
   tamanho = server.arg("tamanho");
   calda = server.arg("calda");
-
-
-
   digitalWrite(ledPin, HIGH); // Ligar o LED na porta 27
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
   display.println("Fazendo pedido...");
-  Serial.print("Fazendo pedido ");
-  Serial.println("==Sabor:");
-
+  Serial.println("Fazendo pedido ");
+  Serial.print("==Sabor:");
   Serial.println(sabor);
-  Serial.println("==Tamanho: ");
+  Serial.print("==Tamanho: ");
   Serial.println(tamanho);
-
-  Serial.println("===Calda: ");
-    Serial.println(calda);
-// enviar para serial 2 as escolhas
-Serial2.print("PS");
-Serial2.print(sabor);
-Serial2.print("T");
-Serial2.print(tamanho);
-Serial2.print("C");
-Serial2.println(calda);
-
+  Serial.print("==Calda: ");
+  Serial.println(calda);
+  // enviar para UNO serial 2 as escolhas acompanhando as letras de inicio PSxTxCx
+  Serial2.print("PS");
+  Serial2.print(sabor);
+  Serial2.print("T");
+  Serial2.print(tamanho);
+  Serial2.print("C");
+  Serial2.println(calda);
   server.send(200, "text/plain", "Informação 1 enviada para o Arduino!");
-
   etapaFimprocesso = false;
   display.display();
   delay(1000); // Aguardar 1 segundo
-  digitalWrite(ledPin, LOW); // Desligar o LED
-  
-  Serial2.println("S"); // Enviar mensagem "chegou" via comunicação serial para o Arduino Uno
- Serial.print("enviando S ");
   server.send(200, "text/plain", "Pedido realizado com sucesso!");
 }
 
 void handlecancelarPedido() {
- digitalWrite(ledPin2, HIGH); // Ligar o LED na porta 27
- delay(1000);
-  digitalWrite(ledPin2, LOW); // Ligar o LED na porta 27
-
+    Serial.print("Cancelar Pedido");
+    // enviar para serial 2 as escolhas
+    Serial2.print("CS");   // comunica com UNO para suspender pedido
+    server.send(200, "text/plain", "Cancelando pedido!");
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("cancelando pedido...");
+    display.display();
 }
 
 void handlemsgoled() {
- server.send(200, "text/html", mensagemPedido); //envia para webpage
-
+   server.send(200, "text/html", mensagemPedido); // atualiza o status no display webpage
 }
 
 //===============================================================
@@ -116,33 +100,13 @@ void handlemsgoled() {
 void setup(void) {
   Serial.begin(serialBaudRate);
   Serial.setTimeout(serialDataTimeout);
-   Serial2.begin(9600, SERIAL_8N1, 16, 17);
-  
-  Serial.println();
-  Serial.println("Booting Sketch...");
- 
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 
-
-  WiFi.mode(WIFI_STA); // Connect to your wifi
-  WiFi.begin(ssid, password);
-
-  Serial.println("Connecting to ");
-  Serial.print(ssid);
-
-  // Wait for WiFi to connect
-  while(WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.print(".");
-  }
-
-  // If connection successful, show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Conectado em SSID: ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  // IP address assigned to your ESP
+  Serial.println("Setup inicial... ");
+  setup_wifi();   // script para conexao wifi
 
   pinMode(ledPin, OUTPUT); // Configure the LED pin 27 as output
-pinMode(ledPin2, OUTPUT); // Configure the LED pin 27 as output
+  pinMode(ledPin2, OUTPUT); // Configure the LED pin 27 as output
 
   // Inicialização do display OLED
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -154,9 +118,9 @@ pinMode(ledPin2, OUTPUT); // Configure the LED pin 27 as output
   server.on("/fazerpedido", handleFazerPedido); // Handle the request for fazer pedido
   server.on("/cancelarpedido", handlecancelarPedido); // Handle the request for cancelar pedido
   server.on("/msgoled", handlemsgoled);
-
-server.begin(); // Start the webserver
-Serial.println("HTTP server started");
+  server.begin(); // Start the webserver
+  Serial.println("HTTP server started");
+  tempoAtual = millis(); // Captura o tempo atual
 }
 
 //===============================================================
@@ -164,124 +128,67 @@ Serial.println("HTTP server started");
 //===============================================================
 
 void loop(void) {
-if (Serial.available()) {
-// Ler dados da comunicação serial e retransmitir para o Arduino Uno
-char data = Serial.read();
-if (data == 'Y') {
-Serial.println(" DIGITOU Y ");
-}
-Serial2.write(data);
-}
+  // Reconnect WiFi if not connected
+  while (WiFi.status() != WL_CONNECTED) {
+      setup_wifi();
+  }  
+  
+  if (Serial.available()) {
+      // Ler dados da comunicação serial e retransmitir para o Arduino Uno
+      char data = Serial.read();
+      if (data == 'Y') {   Serial.println(" DIGITOU Y ");  }
+      Serial2.write(data);
+      }
 
-if (Serial2.available()) {
-// Ler dados do Arduino Uno e retransmitir para a comunicação serial
-char datauno = Serial2.read();
-Serial.write(datauno);
-// Verificar se a mensagem recebida é "chegou"
-   if (datauno == 'Y') {
-   Serial.println(" recebeu do uno Y ");
+  if (Serial2.available()) {
+      // Ler dados do Arduino Uno e retransmitir para a comunicação serial
+      char datauno = Serial2.read();
+      Serial.write(datauno);
+  
+    if (datauno == 'Y') {   Serial.println(" recebeu do uno Y ");   } // Verificar se a mensagem recebida é "chegou"   
+    if (datauno == 'G') {  etapaSorvete = true;  } // Ativar flag  etapa sorvete
+    if (datauno == 'U') {  etapaMotor = true;    } // Ativar a etapa agitação
+    if (datauno == 'T') {  etapaCobertura = true; }    // Ativar a etapa agitação
+    if (datauno == 'L') {  etapaPronto = true;    }     // Ativar a etapa pedido pronto
+    if (datauno == 'F') {  etapaFimprocesso = true;   }      // Ativar a etapa Final
+    if (datauno == 'C') {  pedidoCancelado = true;   }     // Ativar a etapa sorvete
     }
-    
-if (datauno == 'G') {
-etapaSorvete = true; // Ativar a etapa sorvete
  
-}
-if (datauno == 'U') {
-etapaMotor = true; // Ativar a etapa agitação
-}
-if (datauno == 'T') {
-etapaCobertura = true; // Ativar a etapa cobertura
-}
-if (datauno == 'L') {
-etapaPronto = true; // Ativar a etapa pedido pronto
-}
-if (datauno == 'F') {
-etapaFimprocesso = true; // Ativar a etapa Final
-}
-if (datauno == 'C') {
-pedidoCancelado = true; // Ativar a etapa sorvete
-}
-}
- 
-
-// Verificar o estado da etapa sorvete e exibir no display OLED
-
-
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Aguardando pedido...");
-
-mensagemPedido="Aguardando pedido...";
-
-
-if (etapaSorvete) {
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Etapa Sorvete");
-mensagemPedido="Etapa Sorvete";
-} 
-
-if (etapaMotor) {
-  etapaSorvete = false;
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Agitando...");
-mensagemPedido="Agitando...";
-
-}
-if (etapaCobertura) {
-   etapaMotor = false;
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Etapa Calda");
-}
-if (etapaPronto) {
-  etapaCobertura = false;
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Pedido pronto");
-}
-if (etapaFimprocesso) {
-  etapaPronto = false;
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Fim do processo");
-delay(2000);
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Aguardando novo pedido...");
-}
-
-if(pedidoCancelado){
-  display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("pedido cancelado");
-delay(2000);
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(SSD1306_WHITE);
-display.setCursor(0, 0);
-display.println("Aguardando novo pedido...");
-}
-
-display.display();
-
-server.handleClient();
-delay(1);
+    // Verificar o estado da etapa sorvete e exibir no display OLED
+    // Captura o tempo atual
+    if (( tempoAtual - millis())>300) {    // atualiza o display a cada 300ms
+        tempoAtual = millis();
+         display.clearDisplay();
+         display.setTextSize(1);
+         display.setTextColor(SSD1306_WHITE);
+         display.setCursor(0, 0);
+         if ( !etapaSorvete || !etapaMotor || !etapaCobertura || !etapaPronto )  {
+             display.println("Aguardando pedido...");
+             mensagemPedido="Aguardando pedido...";   }
+         if (etapaSorvete) {   
+             display.println("Etapa Sorvete");   
+             mensagemPedido="Etapa Sorvete";    } 
+         if (etapaMotor) {  
+             etapaSorvete = false;   
+             display.println("Agitando...");
+             mensagemPedido="Agitando...";     }
+         if (etapaCobertura) {
+              etapaMotor = false;
+              display.println("Etapa Calda");   }
+         if (etapaPronto) {
+              etapaCobertura = false;
+              display.println("Pedido pronto");  }
+         if (etapaFimprocesso) {
+              etapaPronto = false;
+              display.println("Fim do processo");
+              delay(2000);
+              display.println("Aguardando novo pedido...");   } 
+         if(pedidoCancelado){
+              display.println("pedido cancelado");
+              pedidoCancelado = false;
+              delay(2000);    }
+         display.display();
+       }   // fim atualizacao oled
+       server.handleClient();
+       delay(1);
 }
