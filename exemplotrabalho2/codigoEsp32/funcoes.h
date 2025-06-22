b5a109d69ca7b1f754954704df97d6588aaaf0b6
mchavesferreira <63993080+mchavesferreira@users.noreply.github.com>
2025-06-22 20:01:07 -0300

    Create funcoes.h

---
+++ exemplotrabalho2/codigoEsp32/funcoes.h
@@ -0,0 +1,62 @@
+
+
+
+// Definições da rede WiFi
+const char* ssid = "";  
+const char* password = "";
+
+const char* ssid2 = "Isaweb2";       // Nome da segunda rede Wi-Fi
+const char* password2 = "julia3103"; // Senha da segunda rede Wi-Fi
+
+const char* nomedevice = "esp32_M0007";
+
+
+// Função para esperar pela conexão WiFi com timeout (10 segundos por padrão)
+bool waitForConnection(unsigned long timeout = 10000)
+{
+    unsigned long startTime = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        if (millis() - startTime > timeout)
+        {
+            return false; // Timeout alcançado
+        }
+        delay(500);
+        Serial.print("."); // Mostra progresso da conexão
+    }
+    return true; // Conexão bem-sucedida
+}
+
+void setup_wifi()
+{
+    delay(10);
+    Serial.println();
+
+    // Tenta conectar à primeira rede
+    Serial.print("Tentando conectar a ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    if (waitForConnection())
+    {
+        Serial.println("Conectado à primeira rede!");
+        Serial.print("Endereço IP: ");
+        Serial.println(WiFi.localIP());
+        return; // Se conectou, sai da função
+    }
+
+    // Se falhar, tenta conectar à segunda rede
+    Serial.println("Falha ao conectar à primeira rede. Tentando segunda rede...");
+    WiFi.begin(ssid2, password2);
+    if (waitForConnection())
+    {
+        Serial.println("Conectado à segunda rede!");
+        Serial.print("Endereço IP: ");
+        Serial.println(WiFi.localIP());
+         return;
+    }
+    else
+    {
+        // Se nenhuma conexão for bem-sucedida
+        Serial.println("Falha ao conectar em ambas as redes.");
+    }
+}
